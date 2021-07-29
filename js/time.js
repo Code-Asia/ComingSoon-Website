d7b735ffc0f82f72b354b68454273fd4f1ca3a06
Eddy Zhu <67758739+Tronbo@users.noreply.github.com>
2021-07-29 14:22:49 -0400

    YO YOO

--- js/time.js
+++ js/time.js
@@ -50,7 +50,7 @@ var x = setInterval(function() {
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "January 1, 2022 00:00:00",
+  let birthday = "August 30, 2021 00:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
